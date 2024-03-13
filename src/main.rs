ebc418d72dde00165f87c6b38b6df61f12284940
Gerard <gerardfabrego@gmail.com>
2024-03-13 20:00:43 +0100

    Initialize resizable gl viewport

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,13 @@
-use glfw::{Action, Context, Key};
+use glfw::{Context, OpenGlProfileHint, WindowHint};
 
 fn main() {
     let mut glfw = glfw::init(glfw::fail_on_errors).unwrap();
 
-    let (mut window, events) = glfw
+    glfw.window_hint(WindowHint::ContextVersion(4, 6));
+    glfw.window_hint(WindowHint::OpenGlProfile(OpenGlProfileHint::Core));
+    glfw.window_hint(WindowHint::OpenGlForwardCompat(true));
+
+    let (mut window, _events) = glfw
         .create_window(
             1080,
             720,
@@ -12,19 +16,32 @@ fn main() {
         )
         .expect("Failed to create GLFW window.");
 
-    window.set_key_polling(true);
     window.make_current();
+    window.set_key_polling(true);
+
+    gl::load_with(|s| window.get_proc_address(s) as *const _);
+
+    window.set_framebuffer_size_callback(resize_callback);
 
     while !window.should_close() {
-        glfw.poll_events();
-        for (_, event) in glfw::flush_messages(&events) {
-            handle_window_event(&mut window, event);
+        unsafe {
+            gl::ClearColor(0.3, 0.4, 0.6, 1.0);
+            gl::Clear(gl::COLOR_BUFFER_BIT);
         }
+
+        process_input(&mut window);
+
+        glfw.poll_events();
+        window.swap_buffers();
     }
 }
 
-fn handle_window_event(window: &mut glfw::Window, event: glfw::WindowEvent) {
-    if let glfw::WindowEvent::Key(Key::Escape, _, Action::Press, _) = event {
+fn resize_callback(_: &mut glfw::Window, width: i32, height: i32) {
+    unsafe { gl::Viewport(0, 0, width, height) };
+}
+
+fn process_input(window: &mut glfw::Window) {
+    if window.get_key(glfw::Key::Escape) == glfw::Action::Press {
         window.set_should_close(true);
     }
 }
