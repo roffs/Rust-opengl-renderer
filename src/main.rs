7f9c06cdceba923841cae997ff4a88d88bfe955b
Gerard <gerardfabrego@gmail.com>
2024-03-13 21:50:03 +0100

    Create shader program

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,10 @@
+use std::fs;
+
+use anyhow::{bail, Result};
+
 use glfw::{Context, OpenGlProfileHint, WindowHint};
 
-fn main() {
+fn main() -> Result<()> {
     let mut glfw = glfw::init(glfw::fail_on_errors).unwrap();
 
     glfw.window_hint(WindowHint::ContextVersion(4, 6));
@@ -23,6 +27,13 @@ fn main() {
 
     window.set_framebuffer_size_callback(resize_callback);
 
+    // SHADER PROGRAM
+
+    let vertex_shader = create_shader("src/shaders/shader.vert", gl::VERTEX_SHADER)?;
+    let fragment_shader = create_shader("src/shaders/shader.frag", gl::FRAGMENT_SHADER)?;
+
+    let shader_program = create_shader_program(vertex_shader, fragment_shader)?;
+
     while !window.should_close() {
         unsafe {
             gl::ClearColor(0.3, 0.4, 0.6, 1.0);
@@ -34,6 +45,8 @@ fn main() {
         glfw.poll_events();
         window.swap_buffers();
     }
+
+    Ok(())
 }
 
 fn resize_callback(_: &mut glfw::Window, width: i32, height: i32) {
@@ -44,4 +57,88 @@ fn process_input(window: &mut glfw::Window) {
     if window.get_key(glfw::Key::Escape) == glfw::Action::Press {
         window.set_should_close(true);
     }
+}
+
+fn create_shader(file_path: &str, shader_type: gl::types::GLenum) -> Result<gl::types::GLuint> {
+    let shader_source =
+        fs::read_to_string(file_path).expect("Should have been able to read the file");
+
+    let shader = unsafe { gl::CreateShader(shader_type) };
+
+    let shader_type = match shader_type {
+        gl::VERTEX_SHADER => "Vertex",
+        gl::FRAGMENT_SHADER => "Fragment",
+        _ => "Unspecified type",
+    };
+
+    unsafe {
+        gl::ShaderSource(
+            shader,
+            1,
+            &shader_source.as_bytes().as_ptr().cast(),
+            std::ptr::null(),
+        );
+        gl::CompileShader(shader);
+    }
+
+    let mut success: gl::types::GLint = 1;
+    unsafe {
+        gl::GetShaderiv(shader, gl::COMPILE_STATUS, &mut success);
+    }
+
+    if success == 0 {
+        let mut log_len = 0_i32;
+        let mut info_log: Vec<u8> = Vec::with_capacity(1024);
+
+        unsafe {
+            gl::GetShaderInfoLog(shader, 512, &mut log_len, info_log.as_mut_ptr().cast());
+            info_log.set_len(log_len.try_into().unwrap());
+        }
+
+        bail!(
+            "Error: {} shader from {} compilation failed: {}",
+            shader_type,
+            file_path,
+            String::from_utf8_lossy(&info_log)
+        );
+    }
+
+    println!("{} shader was compiled successfully.", shader_type);
+
+    Ok(shader)
+}
+
+fn create_shader_program(
+    vertex_shader: gl::types::GLenum,
+    fragment_shader: gl::types::GLenum,
+) -> Result<gl::types::GLuint> {
+    let program = unsafe { gl::CreateProgram() };
+
+    unsafe {
+        gl::AttachShader(program, vertex_shader);
+        gl::AttachShader(program, fragment_shader);
+        gl::LinkProgram(program);
+    }
+
+    let mut success: gl::types::GLint = 1;
+    unsafe {
+        gl::GetShaderiv(program, gl::LINK_STATUS, &mut success);
+    }
+
+    if success == 0 {
+        let mut log_len = 0_i32;
+        let mut info_log: Vec<u8> = Vec::with_capacity(1024);
+
+        unsafe {
+            gl::GetProgramInfoLog(program, 512, &mut log_len, info_log.as_mut_ptr().cast());
+            info_log.set_len(log_len.try_into().unwrap());
+        }
+
+        bail!(
+            "Error: Program linking failed: {}",
+            String::from_utf8_lossy(&info_log)
+        );
+    };
+
+    Ok(program)
 }
